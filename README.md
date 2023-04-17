a062c3662d5e795fed71bef8955460dde5baa95c
josephedward <15126922+josephedward@users.noreply.github.com>
2023-04-16 22:02:07 -0400

    docs

--- README.md
+++ README.md
@@ -1,6 +1,27 @@
 # Local K8s Development with CDKTF
-
-
-**
-This is a companion repository for the Hashicorp [Deploy Applications with CDK for Terraform](https://developer.hashicorp.com/terraform/tutorials/cdktf/cdktf-applications) tutorial. 
 The Cloud Development Kit for Terraform (CDKTF) allows you to manage Terraform configuration with code in your preferred programming language.
+
+## FYI
+- This is an example working off this tutorial from Hashicorp: [Deploy Applications with CDK for Terraform](https://developer.hashicorp.com/terraform/tutorials/cdktf/cdktf-applications).
+- Deploys a second stack for the test environment.
+- Utilizes a local Docker registry to store the application image.
+- The application logic is a simplified version of the game 'tetris'. The application is written in TypeScript and compiled to JavaScript.
+- The application is deployed as a Docker container on a local Kubernetes cluster using [kind](https://kind.sigs.k8s.io/).
+- The application is exposed via a NodePort service.
+- The application is deployed to a test environment using a second stack.3
+
+## Prerequisites
+- [NPM (v8.19+)](https://www.npmjs.com/get-npm)
+- [NodeJS (v18+)](https://nodejs.org/en/download/)
+- [Docker](https://docs.docker.com/get-docker/)
+- [Docker Desktop](https://www.docker.com/products/docker-desktop)
+- [Kubectl](https://kubernetes.io/docs/tasks/tools/install-kubectl/)
+- [Kind](https://kind.sigs.k8s.io/docs/user/quick-start/)
+- [Terraform (v1.2+)](https://www.terraform.io/downloads.html)
+- [CDK for Terraform (v0.15+)](https://github.com/cdktf)
+
+## Resources
+- [CDK for Terraform Getting Started](https://learn.hashicorp.com/tutorials/terraform/cdktf-install?in=terraform/cdktf)
+- [CDK for Terraform Tutorial](https://developer.hashicorp.com/terraform/tutorials/cdktf/cdktf-applications)
+- [CDK for Terraform GitHub](https://github.com/hashicorp/terraform-cdk)
+- [CDK for Terraform Documentation](https://www.terraform.io/docs/cdktf/index.html)
--- app/main.ts
+++ app/main.ts
@@ -5,93 +5,6 @@ import * as path from "path"
 import { SimpleKubernetesWebApp, SimpleKubernetesWebAppConfig,} from "./constructs"
   
 
-
-// class MyStack extends TerraformStack {
-//   constructor(scope: Construct, name: string) {
-//     super(scope, name)
-
-//     new kubernetes.provider.KubernetesProvider(this, 'kind', {
-//       configPath: path.join(__dirname, '../kubeconfig.yaml'),
-//     })
-
-//     const app_backend = new SimpleKubernetesWebApp(this, 'app_backend', {
-//       image: 'localhost:5000/nocorp-backend:latest',
-//       replicas: 1,
-//       port: 30002,
-//       app: 'myapp',
-//       component: 'backend',
-//       environment: 'dev',
-//     })
-    
-
-//     new SimpleKubernetesWebApp(this, 'app_frontend', {
-//       image: 'localhost:5000/nocorp-frontend:latest',
-//       replicas: 3,
-//       port: 30001,
-//       app: 'myapp',
-//       component: 'frontend',
-//       environment: 'dev',
-//       env: { BACKEND_APP_URL: `http://localhost:${app_backend.config.port}` },
-//     })
-    
-
-
-//     // new KubernetesWebAppDeployment(this, 'deployment', {
-//     //   image: 'nginx:latest',
-//     //   replicas: 2,
-//     //   app: 'myapp',
-//     //   component: 'frontend',
-//     //   environment: 'dev',
-//     // })
-
-//     // new KubernetesNodePortService(this, 'service', {
-//     //   port: 30001,
-//     //   app: 'myapp',
-//     //   component: 'frontend',
-//     //   environment: 'dev',
-//     // })
-    
-
-//     // new kubernetes.deployment.Deployment(this, "myapp", {
-//     //   metadata: {
-//     //     labels: {
-//     //       app: 'myapp',
-//     //       component: 'frontend',
-//     //       environment: 'dev',
-//     //     },
-//     //     name: 'myapp',
-//     //   },
-//     //   spec: {
-//     //     replicas: '1',
-//     //     selector: {
-//     //       matchLabels: {
-//     //         app: 'myapp',
-//     //         component: 'frontend',
-//     //         environment: 'dev',
-//     //       },
-//     //     },
-//     //     template: {
-//     //       metadata: {
-//     //         labels: {
-//     //           app: 'myapp',
-//     //           component: 'frontend',
-//     //           environment: 'dev',
-//     //         },
-//     //       },
-//     //       spec: {
-//     //         container: [
-//     //           {
-//     //             image: 'nginx:latest',
-//     //             name: 'frontend',
-//     //           },
-//     //         ],
-//     //       },
-//     //     },
-//     //   },
-//     // })
-//   }
-// }
-
 class MyStack extends TerraformStack {
   constructor(
     scope: Construct,
@@ -139,6 +52,7 @@ new MyStack(app, 'app', {
     environment: 'dev',
   },
 })
+
 new MyStack(app, 'app-test', {
   frontend: {
     image: 'localhost:5000/nocorp-frontend:latest',
